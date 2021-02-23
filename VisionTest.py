bfd8d3fbba2a9f03fa8f294d67d705f14dd506b3
treeden <37743981+treeden@users.noreply.github.com>
2021-02-23 12:20:02 -0500

    Added some lines to find contours

--- VisionTest.py
+++ VisionTest.py
@@ -6,14 +6,18 @@ cap.set(3,2000)
 cap.set(4,240)
 
 while True:
-    _, img = cap.read() #cv2.imread("VisionPic.jpg")
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+ #   _, img = cap.read()
+#    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    img = cv2.imread("VisionTest.jpg")
 
     lower_yellow = np.array([23, 60, 83])
     upper_yellow = np.array([30, 255, 255])
-    masking = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
+    masking = cv2.inRange(img, lower_yellow, upper_yellow)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     opening = cv2.morphologyEx(masking, cv2.MORPH_OPEN, kernel)
+    ret, thresh = cv2.threshold(opening, 127, 255, 0)
+    contourImg, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     moment = cv2.moments(opening)
 
@@ -23,6 +27,6 @@ while True:
 
     cv2.circle(opening, (x, y), 15, (35, 175, 242), 10)
 
-    cv2.imshow("Center of the Image", opening)
+    cv2.imshow("Center of the Image", contourImg)
 
     cv2.waitKey(0)
