b83a13c060921bef854fc35d220d789ac95e0017
treeden <37743981+treeden@users.noreply.github.com>
2021-02-27 11:41:28 -0500

    It actually decides what path to go on now

--- VisionTest.py
+++ VisionTest.py
@@ -8,15 +8,15 @@ cap.set(4, 1280)
 # _, img = cap.read()
 img = cv2.imread("Blue-A.jpg")
 
-# hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# lower_yellow = np.array([23, 60, 83])
-# upper_yellow = np.array([30, 255, 255])
-# masking = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
-# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-# opening = cv2.morphologyEx(masking, cv2.MORPH_OPEN, kernel)
-# ret, thresh = cv2.threshold(opening, 127, 255, 0)
-# contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+lower_yellow = np.array([23, 60, 83])
+upper_yellow = np.array([30, 255, 255])
+masking = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+opening = cv2.morphologyEx(masking, cv2.MORPH_OPEN, kernel)
+ret, thresh = cv2.threshold(opening, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 # sortedContours = sorted(contours, key=cv2.contourArea, reverse=True)
 # cnt1 = sortedContours[0]
 # cnt2 = sortedContours[1]
@@ -26,14 +26,30 @@ img = cv2.imread("Blue-A.jpg")
 
 # x = int(moment ["m10"] / moment["m00"])
 
-# y = int(moment ["m01"] / moment["m00"])
+# y = int(moment ["m01"] / moment["m00"])z
 
 height, width, channels = img.shape
 
 centerY = int(height / 2)
 centerX = int(width / 2)
 
-print(height, width)
+contourCenter = []
+checkContour = []
+
+for contour in contours:
+    moment = cv2.moments(contour)
+    contourCenter.append(int(moment["m10"] / moment["m00"]))
+
+for contour in contourCenter:
+    if contour > centerX:
+        checkContour.append(True)
+    else:
+        checkContour.append(False)
+
+if checkContour.count(True) > checkContour.count(False):
+    print("Left")
+else:
+    print("Right")
 
 cv2.circle(img, (centerX, centerY), 15, (35, 175, 242), 10)
 
